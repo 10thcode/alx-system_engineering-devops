2a7147be22766f6a16aead34300f04dbcef56566
attabadukwabena <attabadukwabena@gmail.com>
2023-02-17 05:53:38 +0000

    Edit: 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,9 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		       59, 1, stderr);
+	fwrite(
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	2, 59,
+	stderr);
 	return (1);
 }
