1dc23808361fe48569d57b68be1f13a54128ef32
attabadukwabena <attabadukwabena@gmail.com>
2023-02-22 00:41:57 +0000

    Edit 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char text[] = "_putchar";
+	int i = 0;
+
+	while (text < strlen(text))
+	{
+		_putchar(text[i]);
+		i++;
+	}
 
 	return (0);
 }
