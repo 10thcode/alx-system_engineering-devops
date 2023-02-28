eff1e059e3bd545683e8dfdc8ad367e72b2ea9e1
attabadukwabena <attabadukwabena@gmail.com>
2023-02-28 05:08:58 +0000

    Fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,7 +10,7 @@ void rev_string(char *s)
 {
 	int i;
 	int len = 0;
-	char srev[120];
+	char srev[1200];
 
 	for (i = 0; s[i]; i++)
 	{
@@ -25,6 +25,6 @@ void rev_string(char *s)
 
 	for (i = 0; i <= len; i++)
 	{
-	    s[i] = srev[i];
+		s[i] = srev[i];
 	}
 }
