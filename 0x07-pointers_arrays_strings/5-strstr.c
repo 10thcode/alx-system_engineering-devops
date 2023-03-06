8a7a391cb52abff008d4e2c4fe0a5a40c15bc3a8
attabadukwabena <attabadukwabena@gmail.com>
2023-03-06 16:47:52 +0000

    Add 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * _strstr - locates a substring.
+ * @haystack: string
+ * @needle: substring
+ * Return: a pointer to the beginning
+ * of the located substring, or NULL
+ * if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j = 0;
+	char *substring;
+
+	for (i = 0; needle[i]; i++)
+	{
+		for (j = j; haystack[j]; j++)
+		{
+			printf("%c-%c\n", haystack[j], needle[i]);
+			if (needle[i] == haystack[j])
+			{
+				if (i == 0)
+					substring = &haystack[j];
+				j++;
+				break;
+			}
+			i = 0;
+		}
+		if (haystack[j] == '\0' && i == 0)
+			return (NULL);
+	}
+
+	return (substring);
+}
