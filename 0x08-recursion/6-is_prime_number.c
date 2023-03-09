2e08e74256c587a47bf191cec80bef672d4d58c4
attabadukwabena <attabadukwabena@gmail.com>
2023-03-09 02:10:02 +0000

    Add 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if a number is a prime number
+ * @n: the number
+ * Return: 1 if prime otherwise 0
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	return is_divisible(n, 2);
+}
+
+/**
+ * is_divisible - check if a number is divisible by another number
+ * @n: first number
+ * @i: second number
+ * Return: 0 if divisible otherwise return 1
+ */
+int is_divisible(int n, int i)
+{
+	if (n == i)
+		return 1;
+
+	if (n % i == 0)
+		return 0;
+
+	return (is_divisible(n, i + 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,3 +2,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_root(int n, int i);
+int is_prime_number(int n);
+int is_divisible(int n, int i);
