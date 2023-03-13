c77c50cbf47b78614c2e796c154d4df21140ad39
attabadukwabena <attabadukwabena@gmail.com>
2023-03-13 18:56:55 +0000

    Add 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -31,8 +31,9 @@ int **alloc_grid(int width, int height)
 
 		if (array == NULL)
 		{
+			free(array);
 			for (j = 0; j < i; j++)
-				free(arrays[i]);
+				free(arrays[j]);
 			free(arrays);
 			return (NULL);
 		}
