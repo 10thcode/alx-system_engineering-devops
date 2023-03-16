8a9fec97b822862dc6ff76306ca6349ad1a0754c
attabadukwabena <attabadukwabena@gmail.com>
2023-03-16 22:50:24 +0000

    Add 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,10 +12,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len, i, j;
 	char *s3;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	for (len = 0; s1[len]; len++)
 		;
 
 	s3 = malloc(sizeof(char) * (len + n + 1));
+
+	if (s3 == NULL)
+		return (NULL);
 
 	j = 0;
 	for (i = 0; s1[i]; i++, j++)
