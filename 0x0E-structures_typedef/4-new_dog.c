dc017edf328bc325aa3a0cf17f3b709780272127
attabadukwabena <attabadukwabena@gmail.com>
2023-03-21 05:20:27 +0000

    Edit 4-new-dog.c: freeing all allocated spaces if malloc fails

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,39 +11,55 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	char *owner_cp;
-	char *name_cp;
-	int name_len = 0;
-	int owner_len = 0;
-	int i;
+	char *owner_cp, *name_cp;
+	int name_len = 0, owner_len = 0;
 
 	for (; name[name_len]; name_len++)
 		;
 	for (; owner[owner_len]; owner_len++)
 		;
-
 	name_cp = malloc(sizeof(char) * name_len + 1);
 	if (name_cp == NULL)
+	{
+		free(name_cp);
 		return (NULL);
-
+	}
 	owner_cp = malloc(sizeof(char) * owner_len + 1);
 	if (owner_cp == NULL)
+	{
+		free(name_cp);
+		free(owner_cp);
 		return (NULL);
-
-	for (i = 0; name[i]; i++)
-		name_cp[i] = name[i];
-	name_cp[i] = '\0';
-
-	for (i = 0; owner[i]; i++)
-		owner_cp[i] = owner[i];
-	owner_cp[i] = '\0';
-
+	}
+	copystr(owner, owner_cp);
+	copystr(name, name_cp);
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
+	{
+		free(name_cp);
+		free(owner_cp);
+		free(d);
 		return (NULL);
+	}
 	d->name = name_cp;
 	d->age = age;
 	d->owner = owner_cp;
 
 	return (d);
 }
+
+/**
+ * copystr - copy a string into anothe string variable
+ * @src: source
+ * @dest: destination
+ * Return: void
+ */
+void copystr(char *src, char *dest)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+}
+
