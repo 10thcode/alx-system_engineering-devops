8a883272d752778fdb2a6660fe5e626e718f1e45
attabadukwabena <attabadukwabena@gmail.com>
2023-03-21 02:43:15 +0000

    Edit 1-init_dog.c: Add Return info to init_dog function documentation

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,6 +6,7 @@
  * @name: the name to be given to the dog
  * @age: the age to be give to the dog
  * @owner: the name to be given to the owner of the dog
+ * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
