bd1270933dd1febb6538e6e360142b152ae2622f
attabadukwabena <attabadukwabena@gmail.com>
2023-03-21 02:26:39 +0000

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Name of the owner of the dog
+ *
+ * Description: This structure stores details about a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
