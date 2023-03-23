90791bb187c5ef31d4e58572d40534a9596b1ec8
attabadukwabena <attabadukwabena@gmail.com>
2023-03-23 02:49:22 +0000

    Edit 0-print_name.c: Checking for NULL in args

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
-#include "function_pointers.h"
-
+#include <stddef.h>
 /**
  * print_name - prints a name.
  * @name: a string.
@@ -8,5 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	(*f) (name);
 }
