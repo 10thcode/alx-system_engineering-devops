5724b2fb470c28458739a389495c1eb274c8cc5b
attabadukwabena <attabadukwabena@gmail.com>
2023-03-23 02:52:23 +0000

    Edit 1-array_iterator.c: Checking for NULL args

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,8 +1,8 @@
 #include <stddef.h>
-#include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array.
+ * array_iterator - executes a function given as
+ * a parameter on each element of an array.
  * @array: the array.
  * @size: number of element in the array.
  * @action: the function to be called.
@@ -11,6 +11,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
+	if (array == NULL || size < 1 || action == NULL)
+		return;
 
 	for (i = 0; i < size; i++)
 		(*action) (array[i]);
