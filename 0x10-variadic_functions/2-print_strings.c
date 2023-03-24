b9cc0a1c8d6446636fc4baee2f63542e2a672c50
attabadukwabena <attabadukwabena@gmail.com>
2023-03-24 05:55:53 +0000

    Fix NULL check

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,10 +14,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 	va_list ap;
 
-	if (separator == NULL)
-		return;
 	if (n == 0)
+	{
+		printf("\n");
 		return;
+	}
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
@@ -27,12 +28,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (i == (n - 1))
 				printf("(nil)\n");
 			else
-				printf("(nil)%s", separator);
+				if (separator == NULL)
+					printf("(nil)");
+				else
+					printf("(nil)%s", separator);
 		else
 			if (i == (n - 1))
 				printf("%s\n", str);
 			else
-				printf("%s%s", str, separator);
+				if (separator == NULL)
+					printf("%s", str);
+				else
+					printf("%s%s", str, separator);
 	}
 	va_end(ap);
 }
