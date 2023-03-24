bb14a6f52faf73b8e86a891208aa728297b9ca05
attabadukwabena <attabadukwabena@gmail.com>
2023-03-24 03:43:04 +0000

    Update 1-print_numbers.c
    Check if n is 0.
    Remove separator after last number is printed.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,14 +9,19 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	int i;
+	unsigned int i;
 	va_list ap;
 
 	if (separator == NULL)
 		return;
+	if (n == 0)
+		return;
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
-		printf("%d%s", va_arg(ap, int), separator);
+		if (i == (n - 1))
+			printf("%d\n", va_arg(ap, int));
+		else
+			printf("%d%s", va_arg(ap, int), separator);
 	va_end(ap);
 }
