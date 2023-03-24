e906725ee31db00cff1db0850ea3a341ca398bce
attabadukwabena <attabadukwabena@gmail.com>
2023-03-24 05:48:38 +0000

    Update 1-print_numbers.c: Fix NULL check

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,14 +12,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	if (separator == NULL)
-		return;
-
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 		if (i == (n - 1))
 			printf("%d\n", va_arg(ap, int));
 		else
-			printf("%d%s", va_arg(ap, int), separator);
+			if (separator == NULL)
+				printf("%d", va_arg(ap, int));
+			else
+				printf("%d%s", va_arg(ap, int), separator);
 	va_end(ap);
 }
