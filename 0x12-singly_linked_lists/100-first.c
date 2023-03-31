c6e69e3d4709b90564fdbfe519544192cf2a8044
attabadukwabena <attabadukwabena@gmail.com>
2023-03-31 19:29:49 +0000

    Add first function: A function that executes before main.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * first - execute before main function
+ * Return: void
+ */
+void first(void)
+{
+	printf(
+		"You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n"
+	);
+}
