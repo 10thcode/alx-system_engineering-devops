d0cf131ed50a8ea4a2cc6d8c2c5470ca1fbd1ec5
attabadukwabena <attabadukwabena@gmail.com>
2023-04-10 20:33:09 +0000

    Add free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees a listint_t list.
+ * @head: a pointer to the head of the list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	free_listint(temp);
+	head = NULL;
+}
+
+/**
+ * free_listint - frees a simple listint_t list.
+ * @head: the head of the list
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+
+	if (head->next == NULL)
+	{
+		free(head);
+		return;
+	}
+
+	free_listint(head->next);
+	free(head);
+}
