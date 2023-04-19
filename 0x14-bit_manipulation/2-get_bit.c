ae8d897f8f79ae9e1f21a26da4523d28cfa59a30
attabadukwabena <attabadukwabena@gmail.com>
2023-04-19 08:51:18 +0000

    Update 2-get_bit.c: Checking if index is greater than 32

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * get_bit - gets the value of a bit at a given index.
@@ -9,23 +8,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int len = getlength(n);
-
-	if (index > len)
+	if (index > 32)
 		return (-1);
 
 	return ((n >> index) & 1);
-}
-
-/**
- * getlength - get the length of a binary number
- * @n: the number
- * Return: the length of binary number n.
- */
-int getlength(unsigned long int n)
-{
-	if (n == 0)
-		return (0);
-
-	return (getlength(n >> 1) + 1);
 }
