4a657bc34d85f71d8804d3b0be969357f0fea10d
attabadukwabena <attabadukwabena@gmail.com>
2023-04-19 08:41:51 +0000

    Add getlength function to get the length of binary

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * get_bit - gets the value of a bit at a given index.
@@ -8,5 +9,23 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int len = getlength(n);
+
+	if (index >= len)
+		return (-1);
+
 	return ((n >> index) & 1);
+}
+
+/**
+ * getlength - get the length of a binary number
+ * @n: the number
+ * Return: the length of binary number n.
+ */
+int getlength(unsigned long int n)
+{
+	if (n == 0)
+		return (0);
+
+	return (getlength(n >> 1) + 1);
 }
