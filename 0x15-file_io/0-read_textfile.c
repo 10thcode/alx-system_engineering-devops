5ed59e353cccca202479d1ab0e032f050ec595ca
attabadukwabena <attabadukwabena@gmail.com>
2023-04-21 20:14:12 +0000

    Update 0-read_textfile.c: Optimize code. Using less code.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,8 @@
 #include <unistd.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and
+ * prints it to the POSIX standard output.
  * @filename: the name of the text file
  * @letters: the number of letters it should read and print
  * Return: actual number of letters read and print
@@ -14,31 +15,31 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int file, count;
 	char *str;
 
-	if (filename == NULL)
-		return (0);
-
-	file = open(filename, O_RDONLY);
-
-	if (file == -1)
-		return (0);
-
-	str = malloc(letters);
-
-	if (str == NULL)
+	if (filename != NULL)
 	{
-		free(str);
-		return (0);
+		file = open(filename, O_RDONLY);
+
+		if (file != -1)
+		{
+			str = malloc(letters);
+
+			if (str != NULL)
+			{
+				count = read(file, str, letters);
+
+				if (count != -1)
+				{
+					count = write(STDOUT_FILENO, str, count);
+					close(file);
+
+					return (count);
+				}
+			}
+		}
+
 	}
 
-	count = read(file, str, letters);
+	close(file);
 
-	if (count == -1)
-		return (0);
-
-	count = write(STDOUT_FILENO, str, count);
-
-	if (count == -1)
-		return (0);
-
-	return (count);
+	return (0);
 }
