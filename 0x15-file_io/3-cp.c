77502c64b5ab2356c55b442f6f0deefec4ae85f1
attabadukwabena <attabadukwabena@gmail.com>
2023-04-22 00:36:18 +0000

    Update 3-cp.c: Optimize code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,8 +12,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int src, dest, count;
-	char buffer[1024];
+	int src, dest;
 
 	if (argc != 3)
 	{
@@ -37,13 +36,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	while (1)
-	{
-		count = read(src, buffer, 1024);
-		write(dest, buffer, count);
-		if (count < 1024)
-			break;
-	}
+	cpy(src, dest, argv[1], argv[2]);
 
 	close_fd(dest);
 	close_fd(src);
@@ -62,5 +55,39 @@ void close_fd(int fd)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
 		exit(100);
+	}
+}
+
+/**
+ * cpy - copies the content of a file to another
+ * @src: the source
+ * @dest: the destination
+ * @src_name: name of source file
+ * @dest_name: name of destination file
+ * Return: void
+ */
+void cpy(int src, int dest, char *src_name, char *dest_name)
+{
+	int count;
+	char buffer[1024];
+
+	while (1)
+	{
+		count = read(src, buffer, 1024);
+
+		if (count == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read to %s\n", src_name);
+			exit(98);
+		}
+
+		if (write(dest, buffer, count) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest_name);
+			exit(99);
+		}
+
+		if (count < 1024)
+			break;
 	}
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,5 +5,6 @@ int create_file(const char *filename, char *text_content);
 int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
 void close_fd(int fd);
+void cpy(int src, int dest, char *src_name, char *dest_name);
 
 #endif
