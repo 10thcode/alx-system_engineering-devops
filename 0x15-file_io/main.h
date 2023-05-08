1d26d79f418c1c28d8b33e227dae49adb6c5c9b1
10thcode <attabadukwabena@gmail.com>
2023-05-08 20:10:36 +0000

    Add changes in main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
-void _copy(int file_from, int file_to);
+void _copy(int file_from, int file_to, char *str1, char *str2);
 void _close(int fd);
 
 #endif
