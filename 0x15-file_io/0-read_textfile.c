5135419d5844edf0b677822bc41dcf39fa2e62c2
10thcode <attabadukwabena@gmail.com>
2023-05-08 16:46:00 +0000

    Update read_textfile: checking if letters is negetive number

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t n;
 	char *text;
 
-	if (filename == NULL)
+	if (filename == NULL || letters < 1)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
