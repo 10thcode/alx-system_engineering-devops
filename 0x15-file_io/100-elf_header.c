8c86efb73fdaf05d7d0ec7812b5c9183d957da22
10thcode <attabadukwabena@gmail.com>
2023-05-08 21:37:31 +0000

    :(

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ * main - program entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 if successfull, 0 if unsuccessfull
+ */
+int main(int argc, char *argv[])
+{
+	const char *filename = argv[1];
+	int fd = open(filename, O_RDONLY);
+	Elf64_Ehdr header;
+	ssize_t bytes_read = read(fd, &header, sizeof(header));
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		return (1);
+	}
+
+	if (fd == -1)
+	{
+		perror("Error opening file");
+		return (98);
+	}
+
+	if (bytes_read != sizeof(header))
+	{
+		perror("Error reading ELF header");
+		close(fd);
+		return (98);
+	}
+
+	if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0)
+	{
+		fprintf(stderr, "Error: Not an ELF file\n");
+		close(fd);
+		return (98);
+	}
+
+	print_elf_header(&header);
+
+	close(fd);
+	return (0);
+}
+
+/**
+ * print_elf_header - print elf format
+ * @header: elf header
+ * Return: void
+ */
+void print_elf_header(const Elf64_Ehdr *header)
+{
+	int i;
+
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+
+	for (i = 0; i < EI_NIDENT; ++i)
+		printf("%02x ", header->e_ident[i]);
+
+	printf("\n");
+	printf("  Class:                             %s\n",
+			(header->e_ident[EI_CLASS] == ELFCLASS64) ?
+			"ELF64" : "ELF32");
+	printf("  Data:                              %s\n",
+			(header->e_ident[EI_DATA] == ELFDATA2LSB) ?
+			"2's complement, little endian" :
+			"2's complement, big endian");
+	printf("  Version:                           %d (current)\n",
+			header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            %s\n",
+			(header->e_ident[EI_OSABI] == ELFOSABI_SYSV) ?
+			"UNIX - System V" : "Others");
+	printf("  ABI Version:                       %d\n",
+			header->e_ident[EI_ABIVERSION]);
+	printf("  Type:                              %d (EXEC)\n",
+			header->e_type);
+	printf("  Entry point address:               0x%lx\n",
+			(unsigned long)header->e_entry);
+}
