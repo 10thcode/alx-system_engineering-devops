57320138becee12fb97dd208a363cb035b726388
10thcode <attabadukwabena@gmail.com>
2023-09-26 02:47:17 +0000

    Fix error in 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,5 @@
 # create a manifest that kills a process named killmenow.
 
 exec { 'kill_killmenow':
-  command     => 'pkill killmenow',
-  refreshonly => true,
-  onlyif      => 'pgrep killmenow',
-  subscribe   => File['killmenow'],
+  command => '/usr/bin/pkill killmenow',
 }
